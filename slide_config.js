662b4213f3f685e407c929de51bcc3dbe9e8c08f
Yeonhwa Woo <yhwoo.croute@gmail.com>
2013-06-05 11:31:07 +0900

    index 수정중

--- slide_config.js
+++ slide_config.js
@@ -1,8 +1,8 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    title: 'What is Android',
+    subtitle: '안드로이드에 관련된 이야기들',
     //eventInfo: {
     //  title: 'Google I/O',
     //  date: '6/x/2013'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Yeonhwa Woo',
+    company: 'croute',
+    gplus: 'https://plus.google.com/113131691466488717287/posts',
+    twitter: '@croute17',
+    www: 'http://croute.me',
+    github: 'http://croute.github.io/croute-20130605'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
